5c9956f02548ec8d997bcc38ae34ed0a2bae93cd
Robin Cura <robin.cura@gmail.com>
2018-04-19 11:21:33 +0200

    Correction du chargement du fond de carte pour qu'il accepte les fichiers zips contenant un dossier ou non

--- server.R
+++ server.R
@@ -32,14 +32,18 @@ shinyServer(function(input, output, session) {
   
   observe({
     req(input$shapeInput)
-    oriDir <- getwd()
-    setwd(tempdir())
-    unzip(zipfile = input$shapeInput$datapath, overwrite = TRUE, exdir = "shpdir")
-    fileName <- list.files("shpdir")[1]
-    layerName <- substr(fileName, start = 1, stop = nchar(fileName) - 4)
-    spObject <- readOGR(dsn = "shpdir", layer = layerName, stringsAsFactors = FALSE)
-    setwd(oriDir)
-    baseData$spdf <- spObject
+    #tempDir reste le même sur toute une session
+    # alors que tempfile est différent à chaque fois
+    # donc on pourra explorer plusieurs shapes différents
+    tmpFile <- tempfile()
+    dir.create(tmpFile) # On crée un dossier correspondant au nom du fichier généré
+    unzip(zipfile = input$shapeInput$datapath, overwrite = TRUE, exdir = tmpFile)
+    filePath <- list.files(tmpFile, pattern = ".shp$", recursive = TRUE, full.names = TRUE)
+    if (length(filePath) >= 1){
+      layerName <- sub(x = basename(filePath), pattern = ".shp$", replacement = "")
+      spObject <- readOGR(dsn = filePath, layer = layerName, stringsAsFactors = FALSE)
+      baseData$spdf <- spObject
+    }
   })
   
   
